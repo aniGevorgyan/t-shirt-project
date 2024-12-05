984ccb6caa1ef851503ef15ea9928ffca62e6325
anigevorgyan <ani@brainfors.com>
2024-12-05 11:58:42 +0400

    add upload API

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -10,151 +10,142 @@ import {ActivatedRoute} from "@angular/router";
 import {switchMap} from "rxjs";
 
 @Component({
-  selector: 'app-product',
-  standalone: true,
-  imports: [
-    CommonModule,
-    ColorsComponent,
-    SideComponent,
-    UploadComponent,
-    GapComponent
-  ],
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+    selector: 'app-product',
+    standalone: true,
+    imports: [
+        CommonModule,
+        ColorsComponent,
+        SideComponent,
+        UploadComponent,
+        GapComponent
+    ],
+    templateUrl: './product.component.html',
+    styleUrl: './product.component.scss'
 })
 export class ProductComponent implements OnInit {
-  @ViewChild('vc', {static: true}) vc: ElementRef;
-  public message: string;
-  public imagePath: string;
-  public color: any;
-  public side: any;
-  public textAdded: boolean;
-  public imgArrays: any[] = [];
-  public dragOption: number | null;
-  public product: any;
-  public currentImageUrl: string;
-  public gaps: IGap[] = [];
+    @ViewChild('vc', {static: true}) vc: ElementRef;
+    public message: string;
+    public color: any;
+    public side: any;
+    public textAdded: boolean;
+    public imgArrays: any[] = [];
+    public dragOption: number | null;
+    public product: any;
+    public currentImageUrl: string;
+    public gaps: IGap[] = [];
 
-  constructor(
-      public productService: ProductService,
-      public activateRoute: ActivatedRoute,
-      public cdr: ChangeDetectorRef,
-  ) {}
+    constructor(
+        public productService: ProductService,
+        public activateRoute: ActivatedRoute,
+        public cdr: ChangeDetectorRef,
+    ) {
+    }
 
-  ngOnInit() {
-    this.activateRoute.queryParams
-        .pipe(switchMap((data: any) => this.productService.getById(data.product_id)))
-        .subscribe(res => {
-          if(res.success) {
-            this.product = res;
-            this.side = this.product.print_side[0];
-            this.color = this.product.colors[0];
-            this.setCurrentImage(res);
-            this.gaps = this.gapsArrays();
-          }
-        })
-  }
+    ngOnInit() {
+        this.activateRoute.queryParams
+            .pipe(switchMap((data: any) => this.productService.getById(data.product_id)))
+            .subscribe(res => {
+                if (res.success) {
+                    this.product = res;
+                    this.side = this.product.print_side[0];
+                    this.color = this.product.colors[0];
+                    this.setCurrentImage(res);
+                    this.gaps = this.gapsArrays();
+                }
+            })
+    }
 
-  public setCurrentImage(product: any) {
-    this.currentImageUrl = this.transformToArray(product?.colors[0].images).find((el: any) => el.id == product?.print_side[0].uploaded_file_type_id);
-  }
+    public setCurrentImage(product: any) {
+        this.currentImageUrl = this.transformToArray(product?.colors[0].images).find((el: any) => el.id == product?.print_side[0].uploaded_file_type_id);
+    }
 
-  public transformToArray(massiveObject: any): any {
-    return Object.values(massiveObject);
-  }
+    public transformToArray(massiveObject: any): any {
+        return Object.values(massiveObject);
+    }
 
-  public gapsArrays(): IGap[] {
-    if(this.color?.images) {
-      let gaps = this.transformToArray(this.color?.images).map((el: any) => {
-        return {
-          code: this.product?.print_side.find((el2: any)=> el2.uploaded_file_type_id == el.id)?.id,
-          url: null,
-          height: +el.height,
-          width: +el.width,
-          top: +el.top,
-          left: +el.left,
+    public gapsArrays(): IGap[] {
+        if (this.color?.images) {
+            let gaps = this.transformToArray(this.color?.images).map((el: any) => {
+                return {
+                    code: this.product?.print_side.find((el2: any) => el2.uploaded_file_type_id == el.id)?.id,
+                    url: null,
+                    height: +el.height,
+                    width: +el.width,
+                    top: +el.top,
+                    left: +el.left,
+                }
+            })
+            return gaps;
         }
-      })
-      return gaps;
-    }
-    return [];
-  }
-
-  public onColorChange(e: any) {
-    this.color = e;
-    this.gaps = this.gapsArrays();
-    this.currentImageUrl = this.transformToArray(e.images).find((el: any) => el.id == this.side.uploaded_file_type_id)?.image;
-  }
-
-  public addText() {
-    this.textAdded = true;
-  }
-
-  public onSideChange(e: any) {
-    this.side = e;
-    this.gaps = this.gapsArrays();
-    this.currentImageUrl = this.transformToArray(this.color.images).find((el: any) => el.id == e.uploaded_file_type_id)?.image
-    this.textAdded = false;
-  }
-
-  public onOptionDragStart(event: any, option: any, index: number) {
-    event.dataTransfer.dropEffect = 'move';
-    event.target.style.cursor = 'move';
-    this.dragOption = index;
-    this.showPossibleGaps();
-  }
-
-  public showPossibleGaps() {
-    const gaps = this.vc.nativeElement.getElementsByClassName('gap');
-    Array.from(gaps).forEach((gap: any) => {
-      if (!gap.classList.contains('gap--drag-end')) {
-        gap.classList.add('gap--drag-wait');
-      }
-    });
-  }
-
-  public hidePossibleGaps() {
-    const gaps = this.vc.nativeElement.getElementsByClassName('gap');
-    Array.from(gaps).forEach((gap: any) => {
-      if (gap.classList.contains('gap--drag-wait')) {
-        gap.classList.remove('gap--drag-wait');
-      }
-    });
-  }
-
-  public trackByFn(index: any) {
-    return index.id;
-  }
-
-  public onOptionDragEnd() {
-    this.dragOption = null;
-    this.hidePossibleGaps();
-  }
-
-  public onFileChange(event: any) {
-    this.message = '';
-    const files = event.target.files;
-
-    // this.productService.uploadFile(files[0]).subscribe(res => {})
-
-    if (files.length === 0)
-      return;
-
-    const mimeType = files[0].type;
-    if (mimeType.match(/image\/*/) == null) {
-      this.message = "Only images are supported.";
-      return;
+        return [];
     }
 
-    const reader = new FileReader();
-    this.imagePath = files;
-    reader.readAsDataURL(files[0]);
-    reader.onload = (_event) => {
-      this.imgArrays.push(reader.result);
+    public onColorChange(e: any) {
+        this.color = e;
+        this.gaps = this.gapsArrays();
+        this.currentImageUrl = this.transformToArray(e.images).find((el: any) => el.id == this.side.uploaded_file_type_id)?.image;
     }
-  }
 
-  public deleteImage(index: number) {
-    this.imgArrays = this.imgArrays.filter((el, key) => key !== index);
-  }
+    public addText() {
+        this.textAdded = true;
+    }
+
+    public onSideChange(e: any) {
+        this.side = e;
+        this.gaps = this.gapsArrays();
+        this.currentImageUrl = this.transformToArray(this.color.images).find((el: any) => el.id == e.uploaded_file_type_id)?.image
+        this.textAdded = false;
+    }
+
+    public onOptionDragStart(event: any, option: any, index: number) {
+        event.dataTransfer.dropEffect = 'move';
+        event.target.style.cursor = 'move';
+        this.dragOption = index;
+        this.showPossibleGaps();
+    }
+
+    public showPossibleGaps() {
+        const gaps = this.vc.nativeElement.getElementsByClassName('gap');
+        Array.from(gaps).forEach((gap: any) => {
+            if (!gap.classList.contains('gap--drag-end')) {
+                gap.classList.add('gap--drag-wait');
+            }
+        });
+    }
+
+    public hidePossibleGaps() {
+        const gaps = this.vc.nativeElement.getElementsByClassName('gap');
+        Array.from(gaps).forEach((gap: any) => {
+            if (gap.classList.contains('gap--drag-wait')) {
+                gap.classList.remove('gap--drag-wait');
+            }
+        });
+    }
+
+    public trackByFn(index: any) {
+        return index.id;
+    }
+
+    public onOptionDragEnd() {
+        this.dragOption = null;
+        this.hidePossibleGaps();
+    }
+
+    public onFileChange(event: any) {
+        this.message = '';
+        const files = event.target.files;
+
+        if (files.length === 0)
+            return;
+
+        this.productService.uploadFile(files[0]).subscribe(res => {
+            if (res.success) {
+                this.imgArrays.push(res.url);
+            }
+        })
+    }
+
+    public deleteImage(index: number) {
+        this.imgArrays = this.imgArrays.filter((el, key) => key !== index);
+    }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,4 +1,4 @@
-import {map, Observable} from "rxjs";
+import {Observable} from "rxjs";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 
@@ -9,18 +9,19 @@ import {Injectable} from "@angular/core";
 export class ProductService {
     public mainUrl = 'https://demo.yansprint.com/api';
 
-    public constructor(private http: HttpClient) {}
+    public constructor(private http: HttpClient) {
+    }
 
     public getById(id: string): Observable<any> {
         const url = `${this.mainUrl}/product/${id}`;
         const params = new HttpParams();
-        return this.http.get(url, { params });
+        return this.http.get(url, {params});
     }
 
     public uploadFile(file: File): Observable<any> {
         const url = `${this.mainUrl}/file-preview`;
         const formData = new FormData();
-        formData.append('file', file, file.name);
-        return this.http.post(url, formData);
+        formData.append('file_preview', file, file.name);
+        return this.http.post(url, formData,);
     }
 }
